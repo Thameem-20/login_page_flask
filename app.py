5d28e33e904918eb3a037aa7ffc630470332cf01
Thameem-20 <ahmedthameem20@gmail.com>
2024-01-30 22:52:47 +0530

    seventh commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
@@ -13,6 +13,14 @@ app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -44,7 +52,18 @@ def home():
 @app.route('/login', methods = ['GET','POST'])
 def login():
     form = Loginform()
+    if form.validate_on_submit():
+        user = user.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard.html'))
     return render_template('login.html', form=form)
+
+@app.route('/dashboard', methods = ['GET','POST'] )
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/register', methods = ['GET','POST'])
 def register():
